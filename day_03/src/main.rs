7943f2b27c5fd8f808e099bb63aab26faf44626e
Maximilian Stock <me@maximumstock.net>
2019-12-19 23:59:31 +0100

    Add Day 3

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,77 +1,258 @@
 use std::ops::Range;
 
 fn main() {
-    part1();
-}
-
-fn part1() {
     let (cable1_vectors, cable2_vectors) = input();
-    let mut cable1_coordinates = vectors_to_points(cable1_vectors);
-    let mut cable2_coordinates = vectors_to_points(cable2_vectors);
+    let cable1_points = vectors_to_points(cable1_vectors);
+    let cable2_points = vectors_to_points(cable2_vectors);
 
-    cable1_coordinates.sort();
-    cable2_coordinates.sort();
+    let intersections: Vec<Point> = find_intersections(&mut cable1_points.clone(), &mut cable2_points.clone());
+    let nearest_intersection = intersections.first().unwrap();
 
-    let intersections: Vec<Point> = find_intersections(&cable1_coordinates, &cable2_coordinates);
+    let fastest_intersection = find_fastest_intersection(&intersections, &cable1_points, &cable2_points);
 
-    println!("Found intersections: {:?}", intersections);
+    println!("Part 1: Found intersection: {:?} with distance {:?}", nearest_intersection, nearest_intersection.manhattan_distance());
+    println!("Part 2: Fastest intersection distance: {:?}", fastest_intersection);
 }
 
-type Point = (i32, i32);
+fn find_fastest_intersection(intersections: &Vec<Point>, cable1_points: &Vec<Point>, cable2_points: &Vec<Point>) -> usize {
+    let mut path_lengths = intersections
+        .iter()
+        .map(|int| {
+            get_path_length(&cable1_points, int) + get_path_length(&cable2_points, int)
+        })
+        .collect::<Vec<usize>>();
+    path_lengths.sort();
+    path_lengths.first().unwrap().clone()
+}
 
-fn find_intersections(cable1: &Vec<Point>, cable2: &Vec<Point>) -> Vec<Point> {
-    let mut intersections: Vec<(i32, i32)> = vec![];
-    for p1 in cable1 {
-        for p2 in cable2 {
-            if p1.0 == p2.0 && p1.1 == p2.1 {
-                intersections.push(*p1);
+fn get_path_length(path: &Vec<Point>, point: &Point) -> usize {
+    path.iter().position(|x| x == point).unwrap()
+}
+
+#[derive(Clone, Copy, Debug, Ord, Eq, PartialOrd, PartialEq)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn new(x: isize, y: isize) -> Self {
+        Point { x, y }
+    }
+
+    fn manhattan_distance(&self) -> usize {
+        (isize::abs(self.x) + isize::abs(self.y)) as usize
+    }
+}
+
+fn find_intersections(
+    cable1_points: &mut Vec<Point>,
+    cable2_points: &mut Vec<Point>,
+) -> Vec<Point> {
+    cable1_points.sort();
+    cable2_points.sort();
+
+    let mut intersections = vec![];
+
+    let mut x1 = 0;
+    let mut x2 = 0;
+
+    loop {
+        let cable1_p = cable1_points.get(x1);
+        let cable2_p = cable2_points.get(x2);
+
+        match (cable1_p, cable2_p) {
+            (Some(cp1), Some(cp2)) => {
+                if cp1.x == 0 && cp2.x == 0 && cp1.y == 0 && cp2.y == 0 {
+                    x1 += 1;
+                    x2 += 1;
+                } else if cp1.eq(cp2) {
+                    intersections.push(cp1.clone());
+                    x1 += 1;
+                } else if cp1.gt(cp2) {
+                    x2 += 1;
+                } else {
+                    x1 += 1;
+                }
             }
+            _ => break,
         }
     }
+
+    intersections.sort_by(|left, right| left.manhattan_distance().cmp(&right.manhattan_distance()));
     intersections
 }
 
 fn vectors_to_points(vectors: Vec<&str>) -> Vec<Point> {
-    let mut points: Vec<Point> = vec![(0, 0)];
+    let mut points: Vec<Point> = vec![Point::new(0, 0)];
 
     for vector in vectors {
-        let starting_point: Point = *points.last().unwrap();
-        let (x_range, y_range) = parse_vector(vector);
-        for x in x_range {
-            let new_point = (starting_point.0 + x, starting_point.1);
-            points.push(new_point)
-        }
-        for y in y_range.clone() {
-            let new_point = (starting_point.0, starting_point.1 + y);
-            points.push(new_point)
-        }
+        let starting_point = *points.last().unwrap();
+        let (direction, range) = parse_vector(vector);
+        let mut new_points = match direction {
+            Direction::Up => {
+                range
+                    .map(|delta| Point::new(starting_point.x, starting_point.y + delta))
+                    .collect::<Vec<Point>>()
+            }
+            Direction::Right => {
+                range
+                    .map(|delta| Point::new(starting_point.x + delta, starting_point.y))
+                    .collect::<Vec<Point>>()
+            }
+            Direction::Left => {
+                range
+                    .map(|delta| Point::new(starting_point.x + delta, starting_point.y))
+                    .collect::<Vec<Point>>()
+            }
+            Direction::Down => {
+                range
+                    .map(|delta| Point::new(starting_point.x, starting_point.y + delta))
+                    .collect::<Vec<Point>>()
+            }
+        };
+
+        match direction {
+            Direction::Down | Direction::Left => new_points.reverse(),
+            _ => ()
+        };
+        points.extend(new_points.iter());
     }
 
     points
 }
 
-fn parse_vector(vector: &str) -> (Range<i32>, Range<i32>) {
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn parse_vector(vector: &str) -> (Direction, Range<isize>) {
     let (direction, steps) = vector.split_at(1);
     let steps: i32 = steps.parse().unwrap();
 
     match direction {
-        "U" => (0..0, 1..steps + 1),
-        "D" => (0..0, (-steps)..0),
-        "L" => ((-steps)..0, 0..0),
-        "R" => (1..(steps + 1), 0..0),
-        _ => unreachable!()
+        "U" => (Direction::Up, 1..(steps + 1) as isize),
+        "D" => (Direction::Down, (-steps as isize)..0),
+        "L" => (Direction::Left, (-steps as isize)..0),
+        "R" => (Direction::Right, (1..(steps + 1) as isize)),
+        _ => unreachable!(),
     }
 }
 
 fn input() -> (Vec<&'static str>, Vec<&'static str>) {
     let cable1 = "R1000, U371, R195, U136, R804, U805, L450, U211, R768, U768, L548, U354, L736, U431, L152, U658, L670, D262, L277, U136, L290, U939, R501, U550, L931, D839, R335, D492, L25, U80, R878, U355, R653, U186, R423, D485, L793, D259, L739, U679, R508, D269, R432, D761, R97, D461, L675, U958, L58, U348, L719, D271, R144, U849, R384, U72, L84, U493, R947, U30, L356, D442, R327, U646, R825, U718, L329, D173, L949, D345, L971, D830, L93, U506, R245, D376, R322, D105, L604, D60, R298, D959, L165, D423, R180, D527, R956, D944, R785, U641, L794, D182, R975, D719, L166, U974, L224, U243, L666, U706, R796, D600, L856, D913, L988, D993, L259, U351, R487, D424, L335, U910, L437, D180, R621, D3, R878, D188, R254, D393, L727, U829, R352, U958, L327, D158, L854, D17, R143, D454, R889, D265, L345, U784, R35, D129, R77, U117, R951, D980, L866, U646, R242, D603, L562, U727, L496, U328, L380, D504, R644, U803, L530, D546, R328, D373, L489, U454, R74, D908, R366, U94, R604, D482, L573, D27, R943, U497, L782, D267, L391, U49, R528, D58, R155, D529, R227, D998, R558, D891, R224, U843, R512, U34, R92, U404, R752, U946, L338, D880, L513, D28, L856, D444, L187, U532, L187, U669, L306, U259, R287, D442, R478, U576, R702, U336, L305, U701, L754, D277, R760, D863, L717, U196, L221, U101, L334, D156, L961, D810, L67, D716, L457, D44, L505, D724, R716, D660, L36, D338, R54, U424, R730, U18, L65, D133, R149, U374, R356, D989, R519, U593, L444, D270, R328, U167, L748, D797, L434, U751, R444, D71, R158, D530, L630, U147, R909, D994, L957, U521, L644, D579, R673, U191, R935, U237, R600, D321, L671, U961, L884, U378, R534, D46, R275, U845, R571, U245, L507, U273, R995, U408, L14, D799, L955, D534, R579, D94, R705, D391, R469, D381, R620, U162, R907, D826, R824, U167, L734, U922, L484";
     let cable2 = "L1007, D620, R853, U77, L13, U473, L253, D410, R897, U464, L862, U281, L650, D470, R87, D204, L896, U670, L864, D950, L75, D320, R901, D785, L653, D225, L857, U616, L143, U940, L664, U131, L547, D745, R636, U569, L50, U454, R288, D254, L36, U377, L609, U929, L714, U85, L939, U923, L566, D280, R243, U948, R447, D7, R908, D151, R824, D432, R34, D81, L458, U745, L420, D982, L625, U910, L729, D274, R910, U322, L984, D88, L700, D349, L932, U510, R625, U88, L252, U785, L378, D101, R299, U66, L476, U696, R236, D46, R590, U157, R461, U305, L269, D487, L676, U467, R319, D524, R75, U65, L478, U861, L238, D716, R888, D12, L184, D578, R266, D226, L656, D172, L752, U124, L831, U810, L663, U538, R417, D770, L359, U1, R12, U791, L332, U272, R574, D942, L857, U447, R310, U342, L713, D258, R590, D585, R129, D115, R832, D967, R981, D159, R864, U423, R268, U519, L52, D493, R445, D657, R885, U166, R155, D264, R51, D632, R525, D875, R617, U898, L556, D386, L143, U278, L767, D389, R821, U869, R286, D90, R289, U54, R15, D764, R46, D674, R983, U49, R959, U779, R958, D247, R483, U156, L18, U12, L178, U540, L499, U487, L544, D336, R814, U267, R145, D135, L920, D902, L933, D507, L997, U361, L577, U425, L773, D782, R117, U851, R998, U503, R902, U781, L161, U98, L653, U633, L91, U629, L138, D19, R147, D756, R364, D529, L764, U913, L118, U856, R774, D621, R151, U154, R737, D960, R86, U458, R991, D481, R560, D858, R223, D6, R931, D301, R552, D797, R284, U368, L967, D686, R940, U410, R137, D156, L6, U643, L445, D999, R888, D277, L852, U210, L777, D36, R103, D652, R120, D67, L642, D527, R913, D858, R69, D433, R864, U75, L531, U456, L664, D452, R801, U851, L824, D278, L526, U133, R200, U768, R15, U393, R982, U287, L38, D114, R86, U299, L819, D891, R379, D601, L244";
-    let cable1 = cable1.split(", ").collect::<Vec<&str>>();
-    let cable2 = cable2.split(", ").collect::<Vec<&str>>();
+    let cable1 = parse_cable(cable1);
+    let cable2 = parse_cable(cable2);
     (cable1, cable2)
 }
 
-#[test]
-fn test_vectors_to_points() {
-    assert_eq!(vectors_to_points(vec!["D1", "R1", "L1", "U1"]), vec![(0, 0), (0, -1), (1, -1), (0, -1), (0, 0)])
+fn parse_cable(input: &str) -> Vec<&str> {
+    input.split(",").map(|x| x.trim()).collect::<Vec<&str>>()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_vectors_to_points() {
+        assert_eq!(
+            vectors_to_points(vec!["D1", "R1", "L1", "U3"]),
+            vec![
+                Point::new(0, 0),
+                Point::new(0, -1),
+                Point::new(1, -1),
+                Point::new(0, -1),
+                Point::new(0, 0),
+                Point::new(0, 1),
+                Point::new(0, 2),
+            ]
+        )
+    }
+
+    #[test]
+    fn test_point_ordering() {
+        let mut points = vec![Point::new(0, 2), Point::new(1, 0), Point::new(-5, -1)];
+        let expected = vec![Point::new(-5, -1), Point::new(0, 2), Point::new(1, 0)];
+        points.sort();
+        assert_eq!(points, expected);
+    }
+
+    #[test]
+    fn test_point_equality() {
+        let p1 = Point::new(0, 0);
+        let p2 = Point::new(1, 0);
+        let p3 = Point::new(0, 0);
+
+        assert_ne!(p1, p2);
+        assert_eq!(p1, p3);
+    }
+
+    #[test]
+    fn test_find_intersections() {
+        let input = vec![
+            (
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                135,
+            ),
+            (
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83",
+                159,
+            ),
+        ];
+
+        for (left, right, expected_distance) in input {
+            let cable1 = parse_cable(left);
+            let cable2 = parse_cable(right);
+
+            let mut cable1_points = vectors_to_points(cable1);
+            let mut cable2_points = vectors_to_points(cable2);
+
+            let intersections = find_intersections(&mut cable1_points, &mut cable2_points);
+
+            let first_intersection = intersections.get(0).unwrap();
+            assert_eq!(
+                Point::manhattan_distance(first_intersection),
+                expected_distance
+            );
+        }
+    }
+
+
+    #[test]
+    fn test_find_intersections_small() {
+        let input = vec![
+            (
+                "R3,U3",
+                "U2,R5",
+                5,
+            ),
+            (
+                "L3,D3",
+                "D2,L5",
+                5
+            )
+        ];
+
+        for (left, right, expected_distance) in input {
+            let cable1 = parse_cable(left);
+            let cable2 = parse_cable(right);
+
+            let mut cable1_points = vectors_to_points(cable1);
+            let mut cable2_points = vectors_to_points(cable2);
+
+            let intersections = find_intersections(&mut cable1_points, &mut cable2_points);
+            let first_intersection = intersections.get(0).unwrap();
+            assert_eq!(
+                first_intersection.manhattan_distance(),
+                expected_distance
+            );
+        }
+    }
 }
