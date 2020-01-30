ddbc984300093af83a1172c44515abbd259df07f
Maximilian Stock <me@maximumstock.net>
2020-01-30 11:47:52 +0100

    Refactor Day 11 Grid Implementation

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,7 +1,7 @@
 use intcode::{IntCodeComputer, Memory, MemoryValue, State};
+use lib::Grid;
 use std::collections::HashSet;
 use std::fmt::{Debug, Display, Error, Formatter};
-use std::ops::Add;
 
 fn main() {
     let program = input();
@@ -46,63 +46,6 @@ impl Panel {
 impl Default for Panel {
     fn default() -> Self {
         Panel::Black
-    }
-}
-
-#[derive(Debug)]
-struct Grid<T: Debug> {
-    grid: Vec<T>,
-    grid_size: (usize, usize),
-    offset: usize,
-}
-
-impl<T: Display + Debug> Display for Grid<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        let mut output = String::from("");
-        let (x, y) = self.grid_size;
-
-        for row in (0..y).into_iter() {
-            for col in (0..x).into_iter() {
-                let item = self.grid.get(col * y + row).unwrap();
-                output = output.add(format!("{}", item).as_str());
-            }
-            output = output.add("\n");
-        }
-
-        write!(f, "\n{}", output)
-    }
-}
-
-impl<T: Default + Debug> Grid<T> {
-    fn new(width: usize, height: usize, x_offset: usize, y_offset: usize) -> Grid<T> {
-        let mut data = Vec::with_capacity(width * height);
-
-        for _ in 0..width * height {
-            data.insert(0, T::default())
-        }
-
-        Grid {
-            grid: data,
-            grid_size: (width, height),
-            offset: y_offset * height + x_offset,
-        }
-    }
-}
-
-impl<T: Debug> Grid<T> {
-    fn get(&self, x: isize, y: isize) -> Result<&T, ()> {
-        let (_, dim_y) = self.grid_size;
-        let index = ((y * dim_y as isize + x) + self.offset as isize) as usize;
-        let item = self.grid.get(index).unwrap();
-        Ok(item)
-    }
-
-    fn set(&mut self, x: isize, y: isize, item: T) -> Result<(), ()> {
-        let (_, dim_y) = self.grid_size;
-        let index = ((y * dim_y as isize + x) + self.offset as isize) as usize;
-        let old_item = self.grid.get_mut(index).unwrap();
-        *old_item = item;
-        Ok(())
     }
 }
 
@@ -831,30 +774,4 @@ fn input() -> Memory {
         0,
         0,
     ]
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Grid;
-
-    #[test]
-    fn test_simple_grid() {
-        let mut grid: Grid<bool> = Grid::new(5, 5, 0, 0);
-        assert_eq!(&false, grid.get(2, 3).unwrap());
-        assert_eq!(&false, grid.get(0, 0).unwrap());
-        assert_eq!(&false, grid.get(4, 4).unwrap());
-
-        grid.set(2, 2, true).unwrap();
-        assert_eq!(&true, grid.get(2, 2).unwrap());
-    }
-
-    #[test]
-    fn test_offset_grid() {
-        let mut grid: Grid<bool> = Grid::new(5, 5, 2, 2);
-        assert_eq!(&false, grid.get(-1, -1).unwrap());
-        assert_eq!(&false, grid.get(-2, -2).unwrap());
-
-        grid.set(-2, -2, true).unwrap();
-        assert_eq!(&true, grid.get(-2, -2).unwrap());
-    }
 }
